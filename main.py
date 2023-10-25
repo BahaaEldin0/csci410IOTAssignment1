eb1357ca177ca397951e3caa886be68a665c0139
BahaaEldin0 <b.moustafa@nu.edu.eg>
2023-10-26 00:06:59 +0300

    Added generate fibonacci function

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 def calculate_sum(*args):
     return sum(args)
+
+def generate_fibonacci(N):
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < N:
+        next_number = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_number)
+    return ' '.join(map(str, fib_sequence))
